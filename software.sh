d988ad12696093a3762b6c0a0a052916cec0c953
Richard Wang <leix.wang@outlook.com>
2016-04-07 14:39:05 +0800

    add vim configure.

--- software.sh
+++ software.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install vim  -y
+# vim: vim editor software
+# ibus-libpinyin: pinyin inputer.
+# ibus-table-wubi: wubi inputer.
+
+
+sudo apt-get install vim ibus-libpinyin ibus-table-wubi exuberant-ctags cscope  -y
+
